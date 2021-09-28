254044129360904b789af4857855a6178691f41d
Michael Pierre-Louis <82857756+mgpierrelouis@users.noreply.github.com>
2021-09-28 12:21:17 -0400

    troubleshooting api 404 issue on heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const uri = process.env.MONGODB_URI
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,17 +14,20 @@ app.use(logger("dev"));
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.set('port', process.env.PORT || 3000);
+
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", 
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
+// connect to Mongo database with Mongoose
+mongoose.connect(uri|| 'mongodb://localhost/budget', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}
 );
+
 
 // routes
 app.use(require("./routes/api.js"));
@@ -31,73 +36,6 @@ app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
 
-// const express = require("express");
-// const logger = require("morgan");
-// const mongoose = require("mongoose");
-// const compression = require("compression");
-// const uri = process.env.MONGODB_URI
 
 
-// const PORT = process.env.PORT || 3000;
-
-// const app = express();
-
-// app.use(logger("dev"));
-
-// app.use(compression());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.set('port', process.env.PORT || 3000);
-
-
-// app.use(express.static("public"));
-
-// // connect to Mongo database with Mongoose
-// mongoose.connect(uri|| 'mongodb://localhost/budget', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false
-// }
-// );
-
-
-// // routes
-// app.use(require("./routes/api.js"));
-
-// app.listen(PORT, () => {
-//   console.log(`App running on port ${PORT}!`);
-// });
-
-
-// const express = require("express");
-// const logger = require("morgan");
-// const mongoose = require("mongoose");
-// const compression = require("compression");
-
-// const PORT = proces.ENV.Port || 3000;
-
-// const app = express();
-
-// app.use(logger("dev"));
-
-// const uri = "mongodb+srv://mgpierrelouis:Gwochef_11@offlinebudgettracker.yda4t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-
-// app.use(compression());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// app.use(express.static("public"));
-
-// mongoose.connect(uri|| "mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
-
-
-// app.use(require("./routes/api.js"));
-
-// app.listen(PORT, () => {
-//   console.log(`App running on port ${PORT}!`);
-// });
 
