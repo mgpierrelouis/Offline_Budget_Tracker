8a62dd95cbc2a29bb91f895576d8e44c262cb846
Michael Pierre-Louis <82857756+mgpierrelouis@users.noreply.github.com>
2021-09-28 08:31:40 -0400

    connected index.html to db.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,6 +3,7 @@ const DATA_CACHE_NAME = "data-cache-v1"
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
+    '/db.js',
     '/index.js',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
