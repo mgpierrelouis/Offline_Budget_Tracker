749e739601cd56fb81a4a0854a9754708e9b910f
Michael Pierre-Louis <82857756+mgpierrelouis@users.noreply.github.com>
2021-09-28 12:12:34 -0400

    commented line in server.js to see if that will fix issue w/ heroku deployment.

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const uri = process.env.MONGODB_URI
-
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,20 +12,17 @@ app.use(logger("dev"));
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.set('port', process.env.PORT || 3000);
-
 
 app.use(express.static("public"));
 
-// connect to Mongo database with Mongoose
-mongoose.connect(uri|| 'mongodb://localhost/budget', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-}
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
 );
-
 
 // routes
 app.use(require("./routes/api.js"));
@@ -35,6 +30,44 @@ app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
+// const express = require("express");
+// const logger = require("morgan");
+// const mongoose = require("mongoose");
+// const compression = require("compression");
+// const uri = process.env.MONGODB_URI
+
+
+// const PORT = process.env.PORT || 3000;
+
+// const app = express();
+
+// app.use(logger("dev"));
+
+// app.use(compression());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.set('port', process.env.PORT || 3000);
+
+
+// app.use(express.static("public"));
+
+// // connect to Mongo database with Mongoose
+// mongoose.connect(uri|| 'mongodb://localhost/budget', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false
+// }
+// );
+
+
+// // routes
+// app.use(require("./routes/api.js"));
+
+// app.listen(PORT, () => {
+//   console.log(`App running on port ${PORT}!`);
+// });
 
 
 // const express = require("express");
